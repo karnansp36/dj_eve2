a7cd3789299ed2310515466f0d7a38208dedeb29
karnansp36 <reviewmaster36@gmail.com>
2026-09-21 18:42:50 +0530

    new udpate

--- user_post/models.py
+++ user_post/models.py
@@ -7,3 +7,9 @@ class User_post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+
+class Comments(models.Model):
+    name =models.CharField(max_length=30)
+    desc= models.TextField()
+    create_at= models.DateTimeField(auto_created=True)
